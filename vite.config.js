27260d019cb3add621e535c384a50a27bd87cf68
Abdallah Tourbah <147259166+atourbah01@users.noreply.github.com>
2026-01-09 17:07:21 +0200

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    base: 'https://github.com/atourbah01/memories-book',
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+
+  plugins: [
+    react(),
+  ],
+
+  resolve: {
+    alias: {
+      '@': '/src', // optional but recommended
+    },
+  },
+
+  build: {
+    sourcemap: false,
+  },
+
+  optimizeDeps: {
+    include: [
+      '@mantine/core',
+      '@mantine/hooks',
+      '@tabler/icons-react',
+      'framer-motion',
+    ],
+    esbuildOptions: {
+      sourcemap: false,
+    },
+  },
+});
